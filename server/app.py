d0f5f6343f34aa4372606cedda8fc9c6696d2cf9
Ruth <ruro122020@gmail.com>
2024-03-07 01:08:43 -0500

    users view complete

--- server/app.py
+++ server/app.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
-# Standard library imports
-
-# Remote library imports
-from flask import request
-from flask_restful import Resource
-
 # Local imports
 from config import app
 # Add your model imports
 from routes.routes import *
 from models.models import *
+from dotenv import load_dotenv
 
+#session key configuration
+app.secret_key=b'\xf5\xf6 \x8a\xda\x02|\xad\xe1\xeb\x91\xc3\xf5\xa3\xb7V'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/routes/users.py
+++ server/routes/users.py
@@ -1,7 +1,29 @@
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 from config import app, db, api
 from models.models import User
+from sqlalchemy.exc import IntegrityError
+
+class Signup(Resource):
+  def post(self):
+    json = request.get_json()
+    
+    user = User(
+      name=json.get('name'),
+      username=json.get('username'),
+      email=json.get('email'),
+      image_url=json.get('image_url'))
+    
+    user.password_hash = json.get('password')
+ 
+    try:
+      db.session.add(user)
+      db.session.commit()
+      session['user_id'] = user.id
+      return user.to_dict(), 201
+    except IntegrityError:
+       return {'error': 'Unproccessable Entity'}, 422
+    
 
 
-
+api.add_resource(Signup, '/signup', endpoint='signup')
