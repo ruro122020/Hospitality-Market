cd966dfcfd121e9590b08bb8c30ae17fd7bd13fb
Ruth <ruro122020@gmail.com>
2024-03-07 01:59:48 -0500

    create .env

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import os
 # Local imports
 from config import app
 # Add your model imports
@@ -8,7 +8,8 @@ from models.models import *
 from dotenv import load_dotenv
 
 #session key configuration
-app.secret_key=b'\xf5\xf6 \x8a\xda\x02|\xad\xe1\xeb\x91\xc3\xf5\xa3\xb7V'
+load_dotenv()
+app.secret_key= os.getenv('SECRET_KEY')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
