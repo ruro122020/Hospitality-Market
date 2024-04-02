82f166cd189ccac512577831d1c6cd13506ed5cc
Ruth <ruro122020@gmail.com>
2024-04-01 22:24:25 -0400

    home page complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useAuth } from '../components/contexts/AuthContext'
-
+import Button from '@mui/material/Button'
 const Home = () => {
   const { isLoggedIn } = useAuth()
   return (
-    <div>
-      <h1>Marketplace Services</h1>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      textAlign: 'center',
+      background: 'black',
+      color: 'white',
+      position: 'fixed',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      zIndex: 10
+    }}>
+      <h1 style={{ paddingTop: '60px' }}>Marketplace Services</h1>
       <p>Here we pride ourselves with providing the best services from the best service providers</p>
       <p>Let's get started!</p>
-      <NavLink to={isLoggedIn ? '/services' : '/signup'}>Get Started</NavLink>
+      <Button to={isLoggedIn ? '/services' : '/signup'}>Get Started</Button>
     </div>
 
   )
--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -3,9 +3,11 @@ import { getData } from '../api'
 import ServiceCard from '../components/ServiceCard'
 import { Grid } from '@mui/material'
 import { useUser } from '../components/contexts/UserContext'
+import { useAuth } from '../components/contexts/AuthContext'
 const Services = () => {
   const [services, setServices] = useState(null)
   const { user } = useUser()
+  const { isLoggedIn } = useAuth()
   useEffect(() => {
     const getServices = async () => {
       const services = await getData('/api/services')
@@ -21,9 +23,10 @@ const Services = () => {
 
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome, {user.name}</h1>
-      </div>
+      {isLoggedIn &&
+        <div style={{ textAlign: 'center' }}>
+          <h1>Welcome, {user.name}</h1>
+        </div>}
       <Grid container sx={{ justifyContent: 'space-evenly', paddingTop: '15px' }}>
         {services.map((service) => (
           <ServiceCard key={service.id} service={service} />
